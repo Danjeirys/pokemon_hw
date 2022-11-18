41d63df7ca529c38a014afd00d18bba08e7560cb
Jerry Martinez <jaysavageg@gmail.com>
2022-11-18 16:54:02 -0500

    pokemon

---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+
+app.get ('/', (req, res) => {
+    res.send ('Welcome to Pokemon App!')
+})
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => { 
+    console.log(`Listening on port: ${PORT}`)
+})
