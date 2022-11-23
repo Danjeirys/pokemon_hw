15faf8c839b151b04f56bf5cb573babdb659d69b
Jerry Martinez <jaysavageg@gmail.com>
2022-11-23 00:00:13 -0500

    img aint working

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const Pokemon = require("./models/pokemon");
+const pokemon = require('./models/pokemon')
 const app = express();
 const PORT = 3000;
 const reactViews = require("express-react-views");
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 mongoose.connection.once("open", () => {
-  console.log("connected to mongo");
+    console.log("connected to mongo");
 });
 
 app.set("view engine", "jsx");
 app.engine("jsx", reactViews.createEngine());
 
 app.use((req, res, next) => {
-  next();
+    next();
 });
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Pokemon App!");
+    res.send("Welcome to Pokemon App!");
 });
 
 // app.get ('/pokemon', (req, res) => {
@@ -32,32 +33,32 @@ app.get("/", (req, res) => {
 // })
 
 app.get("/pokemon", (req, res) => {
-  Pokemon.find({}, (error, allPokemon) => {
+Pokemon.find({}, (error, allPokemon) => {
     if (!error) {
-      res.status(200).render("Index", {
+        res.status(200).render("Index", {
         pokemon: allPokemon,
-      });
+    });
     } else {
-      console.log("error");
-      res.status(400).send(error);
+        console.log("error");
+        res.status(400).send(error);
     }
-  });
+    });
 });
 
 app.get("/pokemon/new", (req, res) => {
-  res.render("New");
+    res.render("New");
 });
 
 app.post("/pokemon", (req, res) => {
-  Pokemon.create(req.body, (error, createdPokemon) => {
+Pokemon.create(req.body, (error, createdPokemon) => {
     // console.log(createdPokemon)
     if (!error) {
-      res.status(200).redirect("/pokemon");
+        res.status(200).redirect("/pokemon")
       //res.status(200).send(createdPokemon)
     } else {
-      res.status(400).send(error);
+        res.status(400).send(error);
     }
-  });
+});
 });
 
 // app.get ('/pokemon/:id', (req,res) => {
@@ -65,10 +66,10 @@ app.post("/pokemon", (req, res) => {
 // })
 
 app.get("/pokemon/:id", (req, res) => {
-Pokemon.findById(req.params.id, (error, foundPoke) => {
+Pokemon.findById(req.params.id, (error, foundpoke) => {
     if (!error) {
         res.status(200).render("Show", {
-        pokemon: foundPoke,
+        pokemon: foundpoke,
     });
     } else {
         res.status(400).send(error);
